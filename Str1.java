cfc3ac045a22081846ba2658d86d13c6ff61ce10
Afroz Alam <afrozcpr@gmail.com>
2024-02-29 01:19:08 +0530

    Initial Commit

---
+++ Str1.java
@@ -0,0 +1,14 @@
+public class Str1 {
+    public static void main(String[] args) {
+        String str1="The Best of Both Words";
+        int n=str1.length();
+        int Count=0;
+
+        for(int i=0;i<n;++i){
+            Count++;
+
+        }
+        System.out.println(Count);
+
+    }
+}
